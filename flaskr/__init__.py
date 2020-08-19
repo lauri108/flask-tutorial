bf30a99940d35dffdd029f4887001503736aaf48
Lauri Jalonen <ethereum108@gmail.com>
2020-08-19 22:26:53 +0800

    various HTML templates and CRUD controllers

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,14 +24,18 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
 
-from . import db
-db.init_app(app)
+    from . import db
+    db.init_app(app)
 
-from . import auth
-app.register_blueprint(auth.bp)
+    from . import auth
+    app.register_blueprint(auth.bp)
 
-return app
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
